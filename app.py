eba8a227b878abc0d2f98c3527d2a6982de66aa6
Harsh-2420 <harsh2420@gmail.com>
2021-04-25 11:49:18 +0530

    api changes

--- app.py
+++ app.py
@@ -59,5 +59,62 @@ def upload():
     return 'upload func ran'
 
 
+# @app.route('/authorize')
+# def authorize():
+#   client_id = app.config['CLIENT_ID']
+#   redirect_uri = app.config['REDIRECT_URI']
+#   scope = app.config['SCOPE']
+#   state_key = createStateKey(15)
+#   session['state_key'] = state_key
+
+#   authorize_url = 'https://accounts.spotify.com/en/authorize?'
+#   params = {'response_type': 'code', 'client_id': client_id,
+#             'redirect_uri': redirect_uri, 'scope': scope,
+#             'state': state_key}
+#   query_params = urlencode(params)
+#   response = make_response(redirect(authorize_url + query_params))
+#   return response
+
+
+#   def getToken(code):
+#   token_url = 'https://accounts.spotify.com/api/token'
+#   authorization = app.config['AUTHORIZATION']
+#   redirect_uri = app.config['REDIRECT_URI']
+#   headers = {'Authorization': authorization,
+#              'Accept': 'application/json',
+#              'Content-Type': 'application/x-www-form-urlencoded'}
+#   body = {'code': code, 'redirect_uri': redirect_uri,
+#           'grant_type': 'authorization_code'}
+#   post_response = requests.post(token_url,headers=headers,data=body)
+#   if post_response.status_code == 200:
+#     pr = post_response.json()
+#     return pr['access_token'], pr['refresh_token'], pr['expires_in']
+#   else:
+#     logging.error('getToken:' + str(post_response.status_code))
+#     return None
+
+# def makeGetRequest(session, url, params={}):
+#   headers = {"Authorization": "Bearer {}".format(session['token'])}
+#   response = requests.get(url, headers=headers, params=params)
+#   if response.status_code == 200:
+#     return response.json()
+#   elif response.status_code == 401
+#        and checkTokenStatus(session) != None:
+#     return makeGetRequest(session, url, params)
+#   else:
+#     logging.error('makeGetRequest:' + str(response.status_code))
+#     return None
+
+# def checkTokenStatus(session):
+#   if time.time() > session['token_expiration']:
+#     payload = refreshToken(session['refresh_token'])
+#   if payload != None:
+#     session['token'] = payload[0]
+#     session['token_expiration'] = time.time() + payload[1]
+#   else:
+#     logging.error('checkTokenStatus')
+#     return None
+#   return "Success"
+
 if __name__ == '__main__':
     app.run(debug=True)

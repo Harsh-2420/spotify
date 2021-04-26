5a839901ce5a848e5295a058726378e4472e595a
Harsh-2420 <harsh2420@gmail.com>
2021-04-26 13:58:15 +0530

    changes to flask for api

--- server.py
+++ server.py
@@ -12,6 +12,8 @@ TOKEN_INFO = "token_info"
 
 @app.route('/')
 def login():
+    sp_oauth = create_spotify_ouath()
+    auth_url = sp_oauth.get_authorize_url()
     return "Logging in"
 
 
@@ -23,6 +25,15 @@ def redirectPage():
 @app.route('/getTracks')
 def getTracks():
     return 'some drake songs'
+
+
+def create_spotify_ouath():
+    return SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        # Where to come back to
+        redirect_uri=url_for('redirectPage', _external=True),
+        scope="user-library-read"
 
 
 if __name__ == "__main__":

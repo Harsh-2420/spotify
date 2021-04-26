3f3858b89b503dee9ecccca4244ef1dae03793a1
Harsh-2420 <harsh2420@gmail.com>
2021-04-26 14:02:46 +0530

    api changes

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import time
 from flask import Flask, request, url_for, redirect, session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from git_ignore.config import *
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ TOKEN_INFO = "token_info"
 def login():
     sp_oauth = create_spotify_ouath()
     auth_url = sp_oauth.get_authorize_url()
-    return "Logging in"
+    return redirect(auth_url)
 
 
 @app.route('/redirect')
@@ -33,7 +34,7 @@ def create_spotify_ouath():
         client_secret=client_secret,
         # Where to come back to
         redirect_uri=url_for('redirectPage', _external=True),
-        scope="user-library-read"
+        scope="user-library-read")
 
 
 if __name__ == "__main__":

0f40920b5f6b51dcbc2c0c297d2abdc33eeda97d
Harsh-2420 <harsh2420@gmail.com>
2021-04-28 13:51:18 +0530

    bubble plot

--- server.py
+++ server.py
@@ -4,6 +4,14 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from git_ignore.config import *
 import pandas as pd
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
 
 app = Flask(__name__)
 
@@ -92,6 +100,72 @@ def get_top_artists_data(sp):
             artists[sp_range].append(val)
     top_artists_df = pd.DataFrame(artists)
     return top_artists_df
+
+
+def get_top_albums(sp):
+    ranges = ['short_term', 'medium_term', 'long_term']
+    albums = {}
+    for sp_range in ranges:
+        albums[sp_range] = []
+        results = sp.current_user_top_tracks(time_range=sp_range, limit=20)
+        for i, item in enumerate(results['items']):
+            val = item['album']['name']
+            albums[sp_range].append(val)
+    top_albums_df = pd.DataFrame(tracks)
+    return top_albums_df
+
+
+def plot_top_songs_over_release_date_vs_popularity(sp):
+    tracks = {}
+    tracks['short_term'] = []
+    results = sp.current_user_top_tracks(time_range="short_term", limit=20)
+    for i, item in enumerate(results['items']):
+        song_name = val['name']
+        release_date = sp.album(item["album"]["external_urls"]["spotify"])[
+            'release_date']
+        release_date = datetime.strptime(release_date, "%Y-%m-%d").date()
+        song_popularity = item['popularity']  # int
+        song_duration = item['duration_ms']
+
+        tracks[sp_range].append(val)
+
+    df = px.data.gapminder()
+    fig = px.scatter(df.query("year==2007"), x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent",
+                     hover_name="country", log_x=True, size_max=60)
+    fig.show()
+# def plot_top_albums(df):
+#     return dcc.Graph(
+#         id='top-albums',
+#         figure={
+#             'data': [
+#                 go.Table(
+#                     columnwidth=[35, 20, 11],
+#                     header=dict(
+#                         values=list(
+#                             f"<b>{c}</b>" for c in
+#                             df.columns),
+#                         fill_color='#1759c2',
+#                         align='center',
+#                         height=30,
+#                         font=dict(color='white', size=18)
+#                     ),
+#                     cells=dict(values=[df[c] for c in
+#                                        df.columns],
+#                                # fill_color=['white', 'white', clrs_6_mo, clrs_last_mo],
+#                                line_color='#e1f0e5',
+#                                align='center',
+#                                font=dict(color='black', size=18),
+#                                height=30
+#                                )
+#                 )
+#             ],
+#             'layout': go.Layout(
+#                 margin=dict(t=0, l=0, r=0, b=0),
+#                 height=800
+#             )
+#         }
+#     )
 
 
 if __name__ == "__main__":

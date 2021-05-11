01461d06d7f34ad4e60dd62cc0cb466c553e1952
Harsh-2420 <harsh2420@gmail.com>
2021-05-11 19:23:20 +0530

    bug testing

--- app.py
+++ app.py
@@ -1,125 +1,125 @@
-# import time
-# from flask import Flask, request, url_for, redirect, session, render_template
-# import spotipy
-# from spotipy.oauth2 import SpotifyOAuth
-# from git_ignore.config import *
-# import pandas as pd
-# import dash
-# import dash_table
-# import dash_core_components as dcc
-# import dash_html_components as html
-# from dash.dependencies import Input, Output
-# import plotly.graph_objs as go
-# import plotly.express as px
-# import plotly.figure_factory as ff
-# import json
-# from datetime import datetime
+import time
+from flask import Flask, request, url_for, redirect, session, render_template
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from git_ignore.config import *
+import pandas as pd
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import json
+from datetime import datetime
 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# app.secret_key = "spotty"
-# app.config['SESSION_COOKIE_NAME'] = "Harsh Cookie"
-# TOKEN_INFO = "token_info"
-# PORT = 5000
-
-
-# @app.route('/')
-# def login():
-#     sp_oauth = create_spotify_ouath()
-#     auth_url = sp_oauth.get_authorize_url()
-#     return redirect(auth_url)
+app.secret_key = "spotty"
+app.config['SESSION_COOKIE_NAME'] = "Harsh Cookie"
+TOKEN_INFO = "token_info"
+PORT = 5000
 
 
-# @app.route('/redirect')
-# def redirectPage():
-#     sp_oauth = create_spotify_ouath()
-#     session.clear()
-#     code = request.args.get('code')
-#     token_info = sp_oauth.get_access_token(code)
-#     session[TOKEN_INFO] = token_info
-#     return redirect(url_for('getTracks', _external=True))
+@app.route('/')
+def login():
+    sp_oauth = create_spotify_ouath()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
 
 
-# @app.route('/getTracks')
-# def getTracks():
-#     try:
-#         token_info = get_token()
-#     except:
-#         print("user not logged in")
-#         return redirect('/')
-#     sp = spotipy.Spotify(auth=token_info['access_token'])
-
-#     top_tracks_df = get_top_tracks_data(sp)
-#     # return render_template('base.html',
-#     #                        tables=[top_tracks_df.to_html(
-#     #                            classes='data')],
-#     #                        titles=[top_tracks_df.columns.values])
-#     return render_template(str(top_tracks_df))
+@app.route('/redirect')
+def redirectPage():
+    sp_oauth = create_spotify_ouath()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('getTracks', _external=True))
 
 
-# def get_token():
-#     token_info = session.get(TOKEN_INFO, None)
-#     if not token_info:
-#         raise "exception"
-#     now = int(time.time())
-#     is_expired = token_info['expires_at'] - now < 60
-#     if (is_expired):
-#         sp_oauth = create_spotify_ouath()
-#         token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-#     return token_info
+@app.route('/getTracks')
+def getTracks():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        return redirect('/')
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+
+    top_tracks_df = get_top_tracks_data(sp)
+    # return render_template('base.html',
+    #                        tables=[top_tracks_df.to_html(
+    #                            classes='data')],
+    #                        titles=[top_tracks_df.columns.values])
+    return render_template(str(top_tracks_df))
 
 
-# def create_spotify_ouath():
-#     return SpotifyOAuth(
-#         client_id=client_id,
-#         client_secret=client_secret,
-#         redirect_uri=url_for('redirectPage', _external=True),
-#         scope="user-library-read")
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_ouath()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
 
 
-# def get_top_tracks_data(sp):
-#     ranges = ['short_term', 'medium_term', 'long_term']
-#     tracks = {}
-#     for sp_range in ranges:
-#         tracks[sp_range] = []
-#         results = sp.current_user_top_tracks(time_range=sp_range, limit=20)
-#         for i, item in enumerate(results['items']):
-#             val = item['name']
-#             tracks[sp_range].append(val)
-#     top_tracks_df = pd.DataFrame(tracks)
-#     return top_tracks_df
+def create_spotify_ouath():
+    return SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=url_for('redirectPage', _external=True),
+        scope="user-library-read")
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)  # If Flask App
+def get_top_tracks_data(sp):
+    ranges = ['short_term', 'medium_term', 'long_term']
+    tracks = {}
+    for sp_range in ranges:
+        tracks[sp_range] = []
+        results = sp.current_user_top_tracks(time_range=sp_range, limit=20)
+        for i, item in enumerate(results['items']):
+            val = item['name']
+            tracks[sp_range].append(val)
+    top_tracks_df = pd.DataFrame(tracks)
+    return top_tracks_df
 
 
-# # def get_top_songs_over_release_date_vs_popularity(sp):
+if __name__ == "__main__":
+    app.run(debug=True)  # If Flask App
 
-# #     song_name = []
-# #     release_date = []
-# #     song_popularity = []
-# #     song_duration = []
-# #     artist_name = []
 
-# #     # tracks['short_term'] = []
-# #     results = sp.current_user_top_tracks(time_range="short_term", limit=20)
-# #     for i, item in enumerate(results['items']):
-# #         song_name.append(item['name'])
-# #         date = sp.album(item["album"]["external_urls"]["spotify"])[
-# #             'release_date']
-# #         release_date.append(datetime.strptime(date, "%Y-%m-%d").date().year)
-# #         song_popularity.append(item['popularity'])
-# #         song_duration.append(item['duration_ms'])
-# #         artist_name.append(item['artists'][0]['name'])
+# def get_top_songs_over_release_date_vs_popularity(sp):
 
-# #     # df = pd.DataFrame(
-# #     # {'song_name': song_name,
-# #     #  'song_duration': song_duration,
-# #     #  'song_popularity': song_popularity,
-# #     #  'release_date': release_date,
-# #     #  'artist_name': artist_name
-# #     # })
+#     song_name = []
+#     release_date = []
+#     song_popularity = []
+#     song_duration = []
+#     artist_name = []
 
-# #     return song_name, song_duration, song_popularity, release_date, artist_name
+#     # tracks['short_term'] = []
+#     results = sp.current_user_top_tracks(time_range="short_term", limit=20)
+#     for i, item in enumerate(results['items']):
+#         song_name.append(item['name'])
+#         date = sp.album(item["album"]["external_urls"]["spotify"])[
+#             'release_date']
+#         release_date.append(datetime.strptime(date, "%Y-%m-%d").date().year)
+#         song_popularity.append(item['popularity'])
+#         song_duration.append(item['duration_ms'])
+#         artist_name.append(item['artists'][0]['name'])
+
+#     # df = pd.DataFrame(
+#     # {'song_name': song_name,
+#     #  'song_duration': song_duration,
+#     #  'song_popularity': song_popularity,
+#     #  'release_date': release_date,
+#     #  'artist_name': artist_name
+#     # })
+
+#     return song_name, song_duration, song_popularity, release_date, artist_name

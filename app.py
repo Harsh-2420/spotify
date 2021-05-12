e6123df9bed322ed5a2e40ccdc4374582a0ac413
Harsh-2420 <harsh2420@gmail.com>
2021-05-12 23:04:39 +0530

    split chart 1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import time
 from flask import Flask, request, url_for, redirect, session, render_template
 from recommend import recommend
+from top import top
 from flask.globals import g
 import plotly
 import numpy as np
@@ -21,6 +22,7 @@ import math
 
 app = Flask(__name__)
 app.register_blueprint(recommend, url_prefix="")
+app.register_blueprint(top, url_prefix="")
 
 app.secret_key = "spotty"
 sess = Session()
@@ -68,98 +70,6 @@ def getTracks():
     return render_template('index.html')
 
 
-@app.route('/chart1')
-def chart1():
-    top_tracks_df = session.get('top_tracks', None)
-    top_tracks_df = pd.DataFrame(top_tracks_df)
-
-    # genre_df = session.get('genre_df', None)
-    # genre_df = pd.DataFrame(genre_df)
-
-    popular_df = session.get('popular_df', None)
-    popular_df = pd.DataFrame(popular_df)
-    hover_text = []
-    bubble_size = []
-
-    for index, row in popular_df.iterrows():
-        hover_text.append(('song name: {country}<br>' +
-                           'popularity: {lifeExp}<br>' +
-                           'release: {gdp}<br>' +
-                           'duration: {pop}<br>' +
-                           'artist: {year}').format(country=row['song_name'],
-                                                    lifeExp=row['song_popularity'],
-                                                    gdp=row['release_date'],
-                                                    pop=row['song_duration'],
-                                                    year=row['artist_name']))
-        bubble_size.append(math.sqrt(row['song_duration']))
-    popular_df['text'] = hover_text
-    popular_df['size'] = bubble_size
-    sizeref = 2.*max(popular_df['size'])/(100**2)
-
-    d = dict(tuple(popular_df.groupby('artist_name')))
-
-    fig = go.Figure()
-    fig.add_trace(go.Table(
-        header=dict(values=['long_term', 'medium_term', 'short_term', 'date'],
-                    fill_color='paleturquoise',
-                    align='left'),
-        cells=dict(values=[top_tracks_df.long_term, top_tracks_df.medium_term, top_tracks_df.short_term, popular_df.release_date],
-                   fill_color='lavender',
-                   align='left'))
-                  )
-    fig2 = go.Figure()
-    for artist, data in d.items():
-        fig2.add_trace(go.Scatter(
-            x=data['release_date'], y=data['song_popularity'],
-            name=artist, text=data['text'],
-            marker_size=data['size'],
-        ))
-    fig2.update_traces(mode='markers', marker=dict(sizemode='area',
-                                                   sizeref=sizeref, line_width=2))
-
-    fig2.update_layout(
-        title='Song Popularity vs Release Date',
-        xaxis=dict(
-            title='Release Date',
-            gridcolor='white',
-            # type='',
-            gridwidth=2,
-        ),
-        yaxis=dict(
-            title='Song Popularity',
-            gridcolor='white',
-            gridwidth=2,
-        ),
-        paper_bgcolor='rgb(243, 243, 243)',
-        plot_bgcolor='rgb(243, 243, 243)',
-    )
-
-    # fig3 = px.sunburst(genre_df, path=['parent_genre', 'artist_name_list'])
-    # fig3 = go.Figure()
-    # fig3.add_trace(go.Table(
-    #     header=dict(values=genre_df.columns,
-    #                 fill_color='paleturquoise',
-    #                 align='left'),
-    #     cells=dict(values=[genre_df.parent_genre, genre_df.artist_name_list],
-    #                fill_color='lavender',
-    #                align='left')
-    # ))
-
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
-    # graphJSON3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
-    header = "Get top tweets based on a keyword"
-    description = """
-    Use a given keyword to get most popular tweets. Give option for recent and custom keyword
-    """
-    return render_template('notdash2.html', graphJSON=graphJSON, graphJSON2=graphJSON2, header=header, description=description)
-
-
-# @app.route('/chart2', methods=['GET'])
-# def chart2():
-#     return render_template('recommend.html')
-
-
 @app.route("/predict", methods=['POST'])
 def upload():
     if request.method == "POST":
@@ -192,6 +102,8 @@ def create_spotify_ouath():
         client_secret=client_secret,
         redirect_uri=url_for('redirectPage', _external=True),
         scope="user-top-read user-library-read")
+
+
 
 
 def get_top_tracks_data(sp):
@@ -242,6 +154,7 @@ def get_top_songs_over_release_date_vs_popularity(sp):
     return df
 
 
+
 # def get_genres(sp):
 #     parent_genre = []
 #     artist_name_list = []
@@ -268,36 +181,5 @@ def get_top_songs_over_release_date_vs_popularity(sp):
 #     with open('./pickle/top_genres.pkl', 'rb') as handle:
 #         top_genres = pickle.load(handle)
 #     return top_genres
-
-
-# # recommendationPage
-# # Load the trained model
-# with open('./pickle/cos_sim_results', 'rb') as f:
-#     results = pickle.load(f)
-
-
-# def _recommend(item_id, num):
-#     recs = results[item_id][:num]
-#     preds = {}
-#     for pair in recs:
-#         preds[pair[1]] = pair[0]
-#     return preds
-
-
-# def get_similar_artists_multiple(artists, num=10):
-#     dict_similar = {}
-#     for artist, weight in artists.items():
-#         dict_similar[artist] = _recommend(artist, num)
-#     artists_all = []
-#     for artist, similar_artists in dict_similar.items():
-#         artists_all.append(list(similar_artists.keys()))
-#     artists_unique = np.unique(artists_all).tolist()
-#     artists_dict = {artist: 0 for artist in artists_unique}
-#     for artist, similar_artists in dict_similar.items():
-#         for similar_artist, score in similar_artists.items():
-#             artists_dict[similar_artist] += artists[artist] * score
-#     return list({k: v for k, v in sorted(artists_dict.items(), key=lambda item: item[1], reverse=True) if k not in artists}.keys())[0:num]
-
-
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ top.py
@@ -0,0 +1,98 @@
+from flask import render_template, Blueprint, request, session
+import pickle
+import numpy as np
+import pandas as pd
+import math
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+
+top = Blueprint('top', __name__, template_folder='templates')
+
+
+@top.route('/chart1')
+def chart1():
+    top_tracks_df = session.get('top_tracks', None)
+    top_tracks_df = pd.DataFrame(top_tracks_df)
+
+    # genre_df = session.get('genre_df', None)
+    # genre_df = pd.DataFrame(genre_df)
+
+    popular_df = session.get('popular_df', None)
+    popular_df = pd.DataFrame(popular_df)
+    hover_text = []
+    bubble_size = []
+
+    for index, row in popular_df.iterrows():
+        hover_text.append(('song name: {country}<br>' +
+                           'popularity: {lifeExp}<br>' +
+                           'release: {gdp}<br>' +
+                           'duration: {pop}<br>' +
+                           'artist: {year}').format(country=row['song_name'],
+                                                    lifeExp=row['song_popularity'],
+                                                    gdp=row['release_date'],
+                                                    pop=row['song_duration'],
+                                                    year=row['artist_name']))
+        bubble_size.append(math.sqrt(row['song_duration']))
+    popular_df['text'] = hover_text
+    popular_df['size'] = bubble_size
+    sizeref = 2.*max(popular_df['size'])/(100**2)
+
+    d = dict(tuple(popular_df.groupby('artist_name')))
+
+    fig = go.Figure()
+    fig.add_trace(go.Table(
+        header=dict(values=['long_term', 'medium_term', 'short_term', 'date'],
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[top_tracks_df.long_term, top_tracks_df.medium_term, top_tracks_df.short_term, popular_df.release_date],
+                   fill_color='lavender',
+                   align='left'))
+                  )
+    fig2 = go.Figure()
+    for artist, data in d.items():
+        fig2.add_trace(go.Scatter(
+            x=data['release_date'], y=data['song_popularity'],
+            name=artist, text=data['text'],
+            marker_size=data['size'],
+        ))
+    fig2.update_traces(mode='markers', marker=dict(sizemode='area',
+                                                   sizeref=sizeref, line_width=2))
+
+    fig2.update_layout(
+        title='Song Popularity vs Release Date',
+        xaxis=dict(
+            title='Release Date',
+            gridcolor='white',
+            # type='',
+            gridwidth=2,
+        ),
+        yaxis=dict(
+            title='Song Popularity',
+            gridcolor='white',
+            gridwidth=2,
+        ),
+        paper_bgcolor='rgb(243, 243, 243)',
+        plot_bgcolor='rgb(243, 243, 243)',
+    )
+
+    # fig3 = px.sunburst(genre_df, path=['parent_genre', 'artist_name_list'])
+    # fig3 = go.Figure()
+    # fig3.add_trace(go.Table(
+    #     header=dict(values=genre_df.columns,
+    #                 fill_color='paleturquoise',
+    #                 align='left'),
+    #     cells=dict(values=[genre_df.parent_genre, genre_df.artist_name_list],
+    #                fill_color='lavender',
+    #                align='left')
+    # ))
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    # graphJSON3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+    header = "Get top tweets based on a keyword"
+    description = """
+    Use a given keyword to get most popular tweets. Give option for recent and custom keyword
+    """
+    return render_template('notdash2.html', graphJSON=graphJSON, graphJSON2=graphJSON2, header=header, description=description)

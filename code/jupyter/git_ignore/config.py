2a2695c4611d3575006b677feb0258e0f866507d
Harsh-2420 <harsh2420@gmail.com>
2021-05-14 21:13:40 +0530

    twitter api setup on jupyter

---
+++ code/jupyter/git_ignore/config.py
@@ -0,0 +1,9 @@
+client_id = '28ba49dab9c6456c95496dd9f969c2f3'
+client_secret = '59193af5fdfe4a138f47bca8977eb454'
+
+
+twitter_consumer_key = 'BisqYPaMePUvIDqERLlcMPnwj'
+twitter_consumer_secret = 'yCMvVajrONzZToh8EvSDOiM67AO0HGx7UCwY5oT1ceE3B6Gms1'
+twitter_callback_uri = 'oob'
+twitter_access_token = '1267871380617539585-77Ds7AS0NV8k5ZPeGD54GNLvAsPfEF'
+twitter_access_token_secret = 'JIXkHGaiAXvpY8xl02CACDob76MTbqMHiCMbFX32xLV56'

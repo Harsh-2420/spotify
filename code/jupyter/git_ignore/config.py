1708e3cf8d03a106c9ec016fd954b39a3e375600
Harsh-2420 <harsh2420@gmail.com>
2021-05-15 17:10:29 +0530

    reddit API analysis

--- code/jupyter/git_ignore/config.py
+++ code/jupyter/git_ignore/config.py
@@ -1,9 +1,16 @@
+# Spotify
 client_id = '28ba49dab9c6456c95496dd9f969c2f3'
 client_secret = '59193af5fdfe4a138f47bca8977eb454'
 
-
+# Twitter
 twitter_consumer_key = 'BisqYPaMePUvIDqERLlcMPnwj'
 twitter_consumer_secret = 'yCMvVajrONzZToh8EvSDOiM67AO0HGx7UCwY5oT1ceE3B6Gms1'
 twitter_callback_uri = 'oob'
 twitter_access_token = '1267871380617539585-77Ds7AS0NV8k5ZPeGD54GNLvAsPfEF'
 twitter_access_token_secret = 'JIXkHGaiAXvpY8xl02CACDob76MTbqMHiCMbFX32xLV56'
+
+# Reddit
+reddit_client_secret = 'HeEFguWTJoqVRJ-jlu4Dq3qcQpS4Pg'
+reddit_client_id = 'pmOxZCjGdN_xaw'
+reddit_username = 'Striking_Pineapple2'
+reddit_user_agent = 'redditapi'

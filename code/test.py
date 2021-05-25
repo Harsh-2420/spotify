b28d5e48bd2f24c7139725312e41481c0f23f65e
Harsh-2420 <harsh2420@gmail.com>
2021-05-26 02:45:38 +0530

    bug testing on top pag

--- code/test.py
+++ code/test.py
@@ -12,28 +12,9 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri="http://localhost:5000",
                                                scope=scope))
-# results = sp.current_user_top_artists(time_range='long_term', limit=10)
-# id = results['items'][1]['id']
+results = sp.search('A Day At A Time')
 
-
-results = sp.current_user_top_artists(time_range='long_term', limit=10)
-name = []
-popularity = []
-genres = []
-followers = []
-for item in results['items']:
-    top_id = item['id']
-    recs = sp.artist_related_artists(top_id)
-    for rec in recs['artists']:
-        name.append(rec['name'])
-        popularity.append(rec['popularity'])
-        genres.append(rec['genres'])
-        followers.append(rec['followers']['total'])
-df = pd.DataFrame()
-df['name'] = name
-df['popularity'] = popularity
-df['genres'] = genres
-df['followers'] = followers
-
-
-print(name)
+# for i, item in enumerate(results['items']):
+#     print(item['artists'])
+for result in results['tracks']['items']:
+    print(result["album"]['artists'][0]['name'])

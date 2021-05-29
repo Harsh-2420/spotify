0bb0379672bc3bced86a4232c00aaaee8eb7eaa9
Harsh-2420 <harsh2420@gmail.com>
2021-05-29 15:38:01 +0530

    mongo update method testing

--- code/mongo-test/test.py
+++ code/mongo-test/test.py
@@ -1,9 +1,26 @@
 from pymongo import MongoClient
+import time
 
 cluster = MongoClient(
     "mongodb+srv://admin:asdfgh09@cluster0.h6kse.mongodb.net/spotty?retryWrites=true&w=majority")
 db = cluster["spotty"]
 collection = db['userTracks']
+curr_time = time.time()
 
-for x in collection.find({}):
-    print(x)
+
+def update_db(collection, track, artist, curr_time):
+    query = collection.find(
+        {'track_check': track.lower(), 'artist_check': artist.lower()})
+
+    count = query.count()
+    if count == 0:
+        collection.insert_one({'track': track, 'artist': artist, 'time': curr_time,
+                              'track_check': track.lower(), 'artist_check': artist.lower(), 'count': count})
+    else:
+        for result in query:
+            curr_count = result['count']
+        collection.update_one({'track_check': track.lower()}, {
+            'artist_check': artist.lower}, {'$set': {'count': curr_count}})
+
+
+update_db(collection, 'Ninety', 'Jaden', curr_time)

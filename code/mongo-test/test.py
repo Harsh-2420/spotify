c2c83b0da4fc42fb82eb2fa74d70be87b01b0756
Harsh-2420 <harsh2420@gmail.com>
2021-05-29 14:14:55 +0530

    mongo bug testing

--- code/mongo-test/test.py
+++ code/mongo-test/test.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 
 cluster = MongoClient(
     "mongodb+srv://admin:asdfgh09@cluster0.h6kse.mongodb.net/spotty?retryWrites=true&w=majority")
-
+# cluster = MongoClient('localhost', 27017)
 db = cluster["spotty"]
 collection = db['spotty_user_collection']
 
 collection.insert_one({'name': "JJ"})
+# print(collection)

8391bffbfa72207ebb374c80cf92a6441e51a61e
Harsh-2420 <harsh2420@gmail.com>
2021-05-30 05:52:16 +0530

    mongo plots

--- code/mongo-test/test.py
+++ code/mongo-test/test.py
@@ -1,25 +1,25 @@
-from pymongo import MongoClient
-import time
+# from pymongo import MongoClient
+# import time
 
-cluster = MongoClient(
-    "mongodb+srv://admin:asdfgh09@cluster0.h6kse.mongodb.net/spotty?retryWrites=true&w=majority")
-db = cluster["spotty"]
-collection = db['userTracks']
-curr_time = time.time()
-
-
-def update_db(collection, track, artist, curr_time):
-    query = collection.find(
-        {'track_check': track.lower(), 'artist_check': artist.lower()})
-
-    count = query.count()
-    if count == 0:
-        collection.insert_one({'track': track, 'artist': artist, 'time': curr_time,
-                              'track_check': track.lower(), 'artist_check': artist.lower(), 'count': count})
-    else:
-        for result in query:
-            id = result['_id']
-        collection.find_one_and_update({'_id': id}, {'$inc': {'count': 1}})
+# cluster = MongoClient(
+#     "mongodb+srv://admin:asdfgh09@cluster0.h6kse.mongodb.net/spotty?retryWrites=true&w=majority")
+# db = cluster["spotty"]
+# collection = db['userTracks']
+# curr_time = time.time()
 
 
-update_db(collection, 'Ninety', 'Jaden', curr_time)
+# def update_db(collection, track, artist, curr_time):
+#     query = collection.find(
+#         {'track_check': track.lower(), 'artist_check': artist.lower()})
+
+#     count = query.count()
+#     if count == 0:
+#         collection.insert_one({'track': track, 'artist': artist, 'time': curr_time,
+#                               'track_check': track.lower(), 'artist_check': artist.lower(), 'count': count})
+#     else:
+#         for result in query:
+#             id = result['_id']
+#         collection.find_one_and_update({'_id': id}, {'$inc': {'count': 1}})
+
+
+# update_db(collection, 'Ninety', 'Jaden', curr_time)
--- code/pages/user_collection.py
+++ code/pages/user_collection.py
@@ -1,4 +1,11 @@
 from flask import render_template, Blueprint, request, current_app
+import time
+from datetime import datetime
+import pandas as pd
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+import json
 
 
 user_collection_ = Blueprint(
@@ -7,11 +14,105 @@ user_collection_ = Blueprint(
 
 @user_collection_.route('/user_collection')
 def user_collection():
-    key = request.args.get('key')
-    if key == None:
-        return render_template('user_collection.html')
+    mongo = current_app.config['mongo']
+    collection = mongo.db.userTracks
+    track = request.args.get('track')
+    artist = request.args.get('artist')
+
+    # Initial Run - with no input
+    if track == None or artist == None:
+        track_df = create_df(collection)
+        graphJSON2 = plot_scatter(track_df)
+        return render_template('user_collection.html', graphJSON=graphJSON2)
+    # Secondary Run - with some input provided
     else:
-        mongo = current_app.config['mongo']
-        collection = mongo.db.spotty_user_collection
-        collection.insert_one({'track': key})
-        return render_template('user_collection.html')
+        curr_time = int(time.time())
+        # Check if data is new or existing and update collection
+        update_db(collection, track, artist, curr_time)
+        track_df = create_df(collection)
+        graphJSON2 = plot_scatter(track_df)
+        return render_template('user_collection.html', graphJSON=graphJSON2)
+
+
+def plot_scatter(track_df):
+    hover_text = []
+    # bubble_size = []
+
+    for index, row in track_df.iterrows():
+        hover_text.append(('song name: {country}<br>' +
+                           'count: {lifeExp}<br>' +
+                           'release: {gdp}<br>' +
+                           'artist: {year}').format(country=row['song_name'],
+                                                    lifeExp=row['count'],
+                                                    gdp=row['suggested_date'],
+                                                    year=row['artist_name']))
+        # bubble_size.append(math.sqrt(row['song_duration']))
+    track_df['text'] = hover_text
+    # track_df['size'] = bubble_size
+    # sizeref = 2.*max(track_df['size'])/(100**2)
+    d = dict(tuple(track_df.groupby('artist_name')))
+
+    fig2 = go.Figure()
+    for artist, data in d.items():
+        fig2.add_trace(go.Scatter(
+            x=data['suggested_date'], y=data['count'],
+            name=artist, text=data['text'],
+            # marker_size=data['size'],
+        ))
+        fig2.update_traces(mode='markers', marker=dict(sizemode='area',
+                                                       #    sizeref=sizeref,
+                                                       line_width=2))
+
+    fig2.update_layout(
+        title='Community Recommendations',
+        xaxis=dict(
+            title='Suggested Date',
+            gridcolor='white',
+            gridwidth=2,
+        ),
+        yaxis=dict(
+            title='Count of Recommendations',
+            gridcolor='white',
+            gridwidth=2,
+        ),
+        paper_bgcolor='rgb(243, 243, 243)',
+        plot_bgcolor='rgb(243, 243, 243)',
+    )
+    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON2
+
+
+def update_db(collection, track, artist, curr_time):
+    query = collection.find(
+        {'track_check': track.lower(), 'artist_check': artist.lower()})
+
+    count = query.count()
+    if count == 0:
+        collection.insert_one({'track': track, 'artist': artist, 'time': curr_time,
+                              'track_check': track.lower(), 'artist_check': artist.lower(), 'count': count})
+    else:
+        for result in query:
+            id = result['_id']
+        collection.find_one_and_update({'_id': id}, {'$inc': {'count': 1}})
+
+
+def create_df(collection):
+    song_name = []
+    artist_name = []
+    suggested_date = []
+    song_count = []
+    for result in collection.find({}):
+        song_name.append(result['track'])
+        artist_name.append(result['artist'])
+        time = datetime.utcfromtimestamp(result['time']).strftime('%Y-%m-%d')
+        suggested_date.append(time)
+        song_count = result['count']
+        break
+    df = pd.DataFrame({
+        'song_name': song_name,
+        'artist_name': artist_name,
+        'suggested_date': suggested_date,
+        'count': song_count
+
+    })
+    return df

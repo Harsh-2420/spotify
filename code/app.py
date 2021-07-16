9f8695168fcdef0335cfb9b95ac5e81d9e65c021
Harsh-2420 <harsh2420@gmail.com>
2021-07-16 14:26:16 +0530

    remove old draft code

--- code/app.py
+++ code/app.py
@@ -759,6 +759,9 @@ def contact():
 
 
 
+# ----------------------------End----------------------------
+
+
 if __name__ == '__main__':
     app.run(threaded=True, port=int(os.environ.get("PORT",
                                                    os.environ.get("SPOTIPY_REDIRECT_URI", 5000).split(":")[-1])))

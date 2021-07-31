f5a01fcb2f4acb09d8e03902bb2ce6ed89e62300
Harsh-2420 <harsh2420@gmail.com>
2021-08-01 03:11:30 +0530

    connected flask to mapbox

--- code/app.py
+++ code/app.py
@@ -836,9 +836,9 @@ def world_trends():
     df = pd.read_csv('/Users/harshjhunjhunwala/Desktop/github/spotify/data/shazam.csv')
     data = []
     for row in df.iterrows():
-        data.append([row[1][3], row[1][4], row[1][5].split(',')[0]])
+        data.append([ row[1][5].split(',')[0][2:-1], row[1][4], row[1][3]])
 
-    return render_template('world_trends.html', data=data)
+    return render_template('world_trends.html', data=json.dumps(data))
 
 
 

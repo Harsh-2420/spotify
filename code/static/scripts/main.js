2fe9776bcdf987bcc78424ec25fdcf23bf0f9b6f
Harsh-2420 <harsh2420@gmail.com>
2021-07-31 15:50:37 +0530

    mapbox api setup

--- code/static/scripts/main.js
+++ code/static/scripts/main.js
@@ -1,3 +1,69 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFyc2gtaiIsImEiOiJja3JraHRmMnEzbnA1MndwOGI2OTY1enNrIn0.Lrx1G8lFIKLt_7OsC6ow7g';
+
+var geojson = {
+  'type': 'FeatureCollection',
+  'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [-77.032, 38.913]
+      },
+      'properties': {
+        'title': 'Mapbox',
+        'description': 'Washington, D.C.'
+      }
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [-122.414, 37.776]
+      },
+      'properties': {
+        'title': 'Mapbox',
+        'description': 'San Francisco, California'
+      }
+    }
+  ]
+};
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-96, 37.8],
+  zoom: 3
+});
+
+// add markers to map
+geojson.features.forEach(function (marker) {
+  // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker for each feature and add it to the map
+  new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML(
+          '<h3>' +
+            marker.properties.title +
+            '</h3><p>' +
+            marker.properties.description +
+            '</p>'
+        )
+    )
+    .addTo(map);
+});
+
+
+
+
+
+
+
 (function() {
   "use strict";
 
